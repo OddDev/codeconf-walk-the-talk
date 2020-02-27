8d06d3fc87b4a84c813eab84ca77744622dc15a8
Kai <kai_klostermann@elitemail.org>
2020-02-27 09:42:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-function renderList() {
-  recipelist.innerHTML = ''
-  Object.keys(localStorage).forEach(key => {
-    if (key.startsWith('editor')) {
-      return;
-    }
-    const currentDescription = localStorage.getItem(key)
-    const h3 = document.createElement('h3')
-    const p = document.createElement('p')
-    const button = document.createElement('button')
-    button.addEventListener('click', () => {
-      localStorage.removeItem(key)
-      renderList()
-    })
-    button.append('Remove Recipe')
-    h3.append(key)
-    p.append(currentDescription)
-    const li = document.createElement('li')
-    li.append(h3, p, button)
-    recipeList.append(li)
-  });
-}
+window.addEventListener('DOMContentLoaded', event => {
+  function renderList() {
+    recipelist.innerHTML = ''
+    Object.keys(localStorage).forEach(key => {
+      if (key.startsWith('editor')) {
+        return;
+      }
+      const currentDescription = localStorage.getItem(key)
+      const h3 = document.createElement('h3')
+      const p = document.createElement('p')
+      const button = document.createElement('button')
+      button.addEventListener('click', () => {
+        localStorage.removeItem(key)
+        renderList()
+      })
+      button.append('Remove Recipe')
+      h3.append(key)
+      p.append(currentDescription)
+      const li = document.createElement('li')
+      li.append(h3, p, button)
+      recipeList.append(li)
+    });
+  }
 
-renderList()
-
-addrecipe.addEventListener('click', () => {
-  localStorage.setItem(recipetitle.value, recipedescription.value)
   renderList()
+
+  addrecipe.addEventListener('click', () => {
+    localStorage.setItem(recipetitle.value, recipedescription.value)
+    renderList()
+  })
 })
